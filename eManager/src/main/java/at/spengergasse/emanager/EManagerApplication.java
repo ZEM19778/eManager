ecd384cb3d1a73eb31655610bb1a5f34cb787f6e
ZEM19778 <102022727+ZEM19778@users.noreply.github.com>
2022-11-04 13:38:06 +0100

    Add files via upload

---
+++ eManager/src/main/java/at/spengergasse/emanager/EManagerApplication.java
@@ -0,0 +1,12 @@
+package at.spengergasse.emanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EManagerApplication.class,args);
+    }
+}
