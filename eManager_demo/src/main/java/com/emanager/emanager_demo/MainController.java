2858ebf627958bc84da3c40e572ec469c3ee541d
Tarikbasagic <Bas19428@spengergasse.at>
2022-12-04 23:09:43 +0100

    Mitarbeiterverwaltung (Jusuf&Tarik)
    Erste Admin-Seite erstellt
    Mitarbeiter-Verwaltungs-Seite erstellt
    es läuft jetzt
    aaaaaaaaaalTa

--- eManager_demo/src/main/java/com/emanager/emanager_demo/MainController.java
+++ eManager_demo/src/main/java/com/emanager/emanager_demo/MainController.java
@@ -1,11 +1,16 @@
 package com.emanager.emanager_demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    @Autowired private UserService service;
+
     @GetMapping("/")
     public String start(){
         return "homepageUser";
@@ -30,9 +35,18 @@ public class MainController {
         return "kalender";
     }
 
-
     @GetMapping("/admin")
     public String admin() {
-        return "AdminPage";
+
+        return "homepageAdmin";
     }
+
+    @GetMapping("/userverwaltung")
+    public String userverwaltung(Model model) {
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers",listUsers);
+
+        return "userverwaltung";
+    }
+
 }
