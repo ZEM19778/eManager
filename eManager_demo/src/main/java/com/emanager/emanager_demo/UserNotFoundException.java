c9b58d1a436bdc9a75a520c98cf5167e3ff7bc4c
ZEM19778 <102022727+ZEM19778@users.noreply.github.com>
2022-12-11 20:25:00 +0100

    Delete eManager_demo directory

--- eManager_demo/src/main/java/com/emanager/emanager_demo/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.emanager.emanager_demo;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
